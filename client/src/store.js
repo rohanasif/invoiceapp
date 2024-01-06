df535e5f37554bf436d020fb53dad6fd2c965051
rohanasif <rohanasif1990@gmail..com>
2024-01-06 18:15:19 +0500

    create users slice and add to store

--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import invoicesReducer from "./slice/invoicesSlice";
+import usersReducer from "./slice/usersSlice";
 
 export default configureStore({
   reducer: {
     invoices: invoicesReducer,
+    users: usersReducer,
   },
 });
