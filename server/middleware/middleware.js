39b7e3f438d763a829a6a7198f73e71e11dd2905
rohanasif <rohanasif1990@gmail.com>
2024-01-09 11:43:52 +0500

    correct a typo

--- server/middleware/middleware.js
+++ server/middleware/middleware.js
@@ -1,5 +1,5 @@
 import userModel from "../models/usersModel";
-import { Jwt } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 const users = async (req, res, next) => {
   let token;
@@ -7,7 +7,7 @@ const users = async (req, res, next) => {
   if (authorization && authorization.startsWith("Bearer")) {
     try {
       token = authorization.split(" ")[1];
-      const { userID } = Jwt.verify(token, process.env.JWT_KEY);
+      const { userID } = jwt.verify(token, process.env.JWT_KEY);
       req.user = await userModel.findById(userID).select("-password");
     } catch (error) {
       console.log(error);
