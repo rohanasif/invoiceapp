c1b94a8592d98c3d68cb30bf3ee032ecff83d619
rohanasif <rohanasif1990@mgmail.com>
2024-01-21 09:03:01 +0500

    convert to ternary statement

--- client/src/routes/Dashboard.jsx
+++ client/src/routes/Dashboard.jsx
@@ -10,24 +10,21 @@ const Dashboard = () => {
   const handleLogin = () => {
     navigate("/login");
   };
-  if (token) {
-    return (
-      <div className="flex flex-col items-center justify-center h-screen gap-10">
-        <h1 className="text-4xl">Dashboard</h1>
-        <button onClick={handleLogout} className="p-2 bg-green-700 text-white">
-          Logout
-        </button>
-      </div>
-    );
-  } else {
-    return (
-      <div className="flex flex-col items-center justify-center h-screen gap-10">
-        <h1 className="text-4xl">Dashboard</h1>
-        <button onClick={handleLogin} className="p-2 bg-green-700 text-white">
-          Login
-        </button>
-      </div>
-    );
-  }
+  return token ? (
+    <div className="flex flex-col items-center justify-center h-screen gap-10">
+      <h1 className="text-4xl">Dashboard</h1>
+      <button onClick={handleLogout} className="p-2 bg-green-700 text-white">
+        Logout
+      </button>
+    </div>
+  ) : (
+    <div className="flex flex-col items-center justify-center h-screen gap-10">
+      <h1 className="text-4xl">Dashboard</h1>
+      <button onClick={handleLogin} className="p-2 bg-green-700 text-white">
+        Login
+      </button>
+    </div>
+  );
 };
+
 export default Dashboard;
