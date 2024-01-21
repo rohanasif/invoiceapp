dbd97c3508995a7ba238bf12e164114c52f1c9fd
rohanasif <rohanasif1990@gmail.com>
2024-01-21 22:40:04 +0500

    modal function complete

--- client/src/components/NewCustomerModal.jsx
+++ client/src/components/NewCustomerModal.jsx
@@ -18,6 +18,7 @@ const NewCustomerModal = ({ setNewCustomerModal }) => {
   const addCustomer = (e) => {
     e.preventDefault();
     createCustomer(customer);
+    closeModal();
   };
   return (
     <div className="absolute h-screen w-full">
@@ -28,60 +29,70 @@ const NewCustomerModal = ({ setNewCustomerModal }) => {
         >
           X
         </button>
-        <div className="bg-white h-1/2 w-1/2 rounded-2xl p-4 flex flex-col">
+        <div className="bg-white h-2/3 w-1/2 rounded-2xl p-4 flex flex-col">
           <h2 className="text-3xl">New Customer</h2>
-          <form className="flex flex-wrap" onSubmit={addCustomer}>
-            <div className="flex">
+          <form
+            className="flex flex-col flex-wrap items-center"
+            onSubmit={addCustomer}
+          >
+            <div className="flex flex-col">
               <label htmlFor="name">Name</label>
               <input
                 type="text"
                 name="name"
                 id="name"
                 value={customer.name}
+                className="focus:outline-none border-2 border-gray-400"
                 onChange={(e) =>
                   setCustomer({ ...customer, name: e.target.value })
                 }
               />
             </div>
-            <div className="flex">
+            <div className="flex flex-col">
               <label htmlFor="email">Email</label>
               <input
                 type="email"
                 name="email"
                 id="email"
                 value={customer.email}
+                className="focus:outline-none border-2 border-gray-400"
                 onChange={(e) =>
                   setCustomer({ ...customer, email: e.target.value })
                 }
               />
             </div>
-            <div className="flex">
+            <div className="flex flex-col">
               <label htmlFor="phone">Phone</label>
               <input
                 type="text"
                 name="phone"
                 id="phone"
                 value={customer.phone}
+                className="focus:outline-none border-2 border-gray-400"
                 onChange={(e) =>
                   setCustomer({ ...customer, phone: e.target.value })
                 }
               />
             </div>
-            <div className="flex">
+            <div className="flex flex-col">
               <label htmlFor="address">Address</label>
               <input
                 type="text"
                 name="address"
                 id="address"
                 value={customer.address}
+                className="focus:outline-none border-2 border-gray-400"
                 onChange={(e) =>
                   setCustomer({ ...customer, address: e.target.value })
                 }
               />
             </div>
-            <div className="flex">
-              <button type="submit">Save Customer</button>
-            </div>
+            <button
+              className="px-4 py-2 bg-green-700 text-white mt-4"
+              type="submit"
+            >
+              Save Customer
+            </button>
           </form>
         </div>
       </div>
